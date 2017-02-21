fd596940fbf7518c0146ca212c674b89b732f5c1
Erik Hauner <ehauner@gmail.com>
2017-02-20 23:18:46 -0800

    Added map and places search (not working yet)

--- inukshukApp/index.android.js
+++ inukshukApp/index.android.js
@@ -5,49 +5,103 @@
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, Navigator, View, StyleSheet } from 'react-native';
 
-export default class inukshukApp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+import MapView from 'react-native-maps';
+var {GooglePlacesAutocomplete} = require('react-native-google-places-autocomplete');
+
+const homePlace = {description: 'Home', geometry: { location: { lat: 48.8152937, lng: 2.4597668 } }};
+const workPlace = {description: 'Work', geometry: { location: { lat: 48.8496818, lng: 2.2940881 } }};
+
+ class inukshukApp extends Component {
+   render() {
+     return (
+       <View style={styles.container}>
+
+         <MapView
+           style={styles.map}
+           region={{
+             latitude: 37.78825,
+             longitude: -122.4324,
+             latitudeDelta: 0.015,
+             longitudeDelta: 0.0121,
+           }}
+         >
+         <MapView.Marker
+           coordinate={{
+           latitude: 37.78825,
+           longitude: -122.4324,
+           latitudeDelta: 0.015,
+           longitudeDelta: 0.0121,
+           }}
+           draggable={true}
+         />
+         </MapView>
+
+         <GooglePlacesAutocomplete
+          placeholder='Search'
+          minLength={2} // minimum length of text to search
+          autoFocus={false}
+          listViewDisplayed='auto'    // true/false/undefined
+          fetchDetails={true}
+          renderDescription={(row) => row.description} // custom description render
+          onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
+            console.log(data);
+            console.log(details);
+          }}
+          getDefaultValue={() => {
+            return ''; // text input default value
+          }}
+          query={{
+            // available options: https://developers.google.com/places/web-service/autocomplete
+            key: 'AIzaSyD2xZoo-Et69gVY2ot-uwhKcqvQ4e_FP6s',
+            language: 'en', // language of the results
+            types: '(cities)', // default: 'geocode'
+          }}
+          styles={{
+            description: {
+              fontWeight: 'bold',
+            },
+            predefinedPlacesDescription: {
+              color: '#1faadb',
+            },
+            textInputContainer: {
+              backgroundColor: 'white',
+            }
+          }}
+          style={styles.search}
+
+          currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
+          currentLocationLabel="Current location"
+          nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+          GoogleReverseGeocodingQuery={{
+            // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
+          }}
+          GooglePlacesSearchQuery={{
+            // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+            rankby: 'distance',
+            types: 'food',
+          }}
+
+          filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+
+          predefinedPlaces={[homePlace, workPlace]}
+        />
+       </View>
+     )
+   }
+ }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+   container: {
+     ...StyleSheet.absoluteFillObject,
+     height: 400,
+     justifyContent: 'flex-end',
+     alignItems: 'center',
+   },
+   map: {
+     ...StyleSheet.absoluteFillObject,
+   },
 });
 
 AppRegistry.registerComponent('inukshukApp', () => inukshukApp);

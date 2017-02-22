c742d0bc00229eaa36773261131826d43d5723e7
Erik Hauner <ehauner@gmail.com>
2017-02-22 00:37:22 -0800

    Implemented new search, center on GPS position, update latlon on map move

--- inukshukApp/index.android.js
+++ inukshukApp/index.android.js
@@ -5,102 +5,183 @@
  */
 
 import React, { Component } from 'react';
-import { AppRegistry, Navigator, View, StyleSheet } from 'react-native';
+import { AppRegistry, Navigator, View, StyleSheet, TouchableOpacity, Text, PropTypes, Dimensions, Image, Button, Alert } from 'react-native';
 
 import MapView from 'react-native-maps';
-var {GooglePlacesAutocomplete} = require('react-native-google-places-autocomplete');
+import RNGooglePlaces from 'react-native-google-places';
 
-const homePlace = {description: 'Home', geometry: { location: { lat: 48.8152937, lng: 2.4597668 } }};
-const workPlace = {description: 'Work', geometry: { location: { lat: 48.8496818, lng: 2.2940881 } }};
+var { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+
+// (Initial Static Location) Mumbai
+const LATITUDE = 19.0760;
+const LONGITUDE = 72.8777;
+
+const LATITUDE_DELTA = 0.01;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
  class inukshukApp extends Component {
-   render() {
-     return (
-       <View style={styles.container}>
 
-         <MapView
-           style={styles.map}
-           region={{
-             latitude: 37.78825,
-             longitude: -122.4324,
-             latitudeDelta: 0.015,
-             longitudeDelta: 0.0121,
-           }}
-         >
-         <MapView.Marker
-           coordinate={{
-           latitude: 37.78825,
-           longitude: -122.4324,
-           latitudeDelta: 0.015,
-           longitudeDelta: 0.0121,
-           }}
-           draggable={true}
-         />
-         </MapView>
-
-         <GooglePlacesAutocomplete
-          placeholder='Search'
-          minLength={2} // minimum length of text to search
-          autoFocus={false}
-          listViewDisplayed='auto'    // true/false/undefined
-          fetchDetails={true}
-          renderDescription={(row) => row.description} // custom description render
-          onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
-            console.log(data);
-            console.log(details);
-          }}
-          getDefaultValue={() => {
-            return ''; // text input default value
-          }}
-          query={{
-            // available options: https://developers.google.com/places/web-service/autocomplete
-            key: 'AIzaSyD2xZoo-Et69gVY2ot-uwhKcqvQ4e_FP6s',
-            language: 'en', // language of the results
-            types: '(cities)', // default: 'geocode'
-          }}
-          styles={{
-            description: {
-              fontWeight: 'bold',
-            },
-            predefinedPlacesDescription: {
-              color: '#1faadb',
-            },
-            textInputContainer: {
-              backgroundColor: 'white',
-            }
-          }}
-          style={styles.search}
-
-          currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
-          currentLocationLabel="Current location"
-          nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
-          GoogleReverseGeocodingQuery={{
-            // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
-          }}
-          GooglePlacesSearchQuery={{
-            // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
-            rankby: 'distance',
-            types: 'food',
-          }}
-
-          filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
-
-          predefinedPlaces={[homePlace, workPlace]}
-        />
-       </View>
-     )
+   constructor(props) {
+     super(props);
+     this.onRegionChange = this.onRegionChange.bind(this);
+     this.centerLocation = this.centerLocation.bind(this);
+     this.state = {
+       region: {
+         latitude: LATITUDE,
+         longitude: LONGITUDE,
+         latitudeDelta: LATITUDE_DELTA,
+         longitudeDelta: LONGITUDE_DELTA,
+       },
+       initialRegion: {
+         latitude: LATITUDE,
+         longitude: LONGITUDE,
+         latitudeDelta: LATITUDE_DELTA,
+         longitudeDelta: LONGITUDE_DELTA,
+       }
+     };
    }
+  //
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          initialRegion: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          },
+        });
+      },
+      (error) => alert(JSON.stringify(error)),
+      {enableHighAccuracy: false, timeout: 20000, maximumAge: 1000}
+    );
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      this.setState({
+        region: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        },
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+
+  onRegionChange(region) {
+    this.setState({ region });
+  };
+
+  centerLocation() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position);
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          },
+        });
+      },
+      (error) => alert(JSON.stringify(error)),
+      {enableHighAccuracy: false, timeout: 20000, maximumAge: 1000}
+    );
+  }
+
+   openSearchModal() {
+   RNGooglePlaces.openAutocompleteModal()
+    .then((place) => {
+      this.setState({
+        region: {
+          latitude: place.latitude,
+          longitude: place.longitude,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        },
+      })
+        // console.log(place);
+        // console.log(this.state.region);
+    })
+    .catch(error => console.log(error.message));  // error is a Javascript Error object
+  }
+  //
+  render() {
+    return (
+      <View style={styles.container}>
+      <MapView
+          style={styles.map}
+          region={this.state.region}
+          onRegionChange={this.onRegionChange}
+        >
+        </MapView>
+        <TouchableOpacity
+          style={styles.buttonContainer}
+        >
+          <Button
+            style={styles.button}
+            onPress={this.centerLocation}
+            title="Center"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+          />
+          <Button
+            style={styles.button}
+            onPress={() => this.openSearchModal()}
+            title="Search"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+          />
+          <Button
+            style={styles.button}
+            onPress={() => Alert.alert('Submited ' + this.state.region.latitude.toPrecision(7) + ", " + this.state.region.longitude.toPrecision(7))}
+            title="Submit"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+          />
+      </TouchableOpacity>
+        <View style={styles.markerContainer}>
+          <Image style={styles.marker} source={require('./marker.png')} />
+        </View>
+        <TouchableOpacity>
+          <Text style={{ textAlign: 'center'}}>
+            {this.state.region.latitude.toPrecision(7)}, {this.state.region.longitude.toPrecision(7)}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
  }
 
 const styles = StyleSheet.create({
    container: {
      ...StyleSheet.absoluteFillObject,
-     height: 400,
-     justifyContent: 'flex-end',
+     justifyContent: 'center',
      alignItems: 'center',
    },
    map: {
      ...StyleSheet.absoluteFillObject,
+   },
+   markerContainer: {
+     flex: 1,
+     justifyContent: 'center',
+     alignItems: 'center',
+   },
+   marker: {
+     width: 25,
+     height: 40,
+     position: 'absolute',
+     left: -12.5,
+   },
+   buttonContainer: {
+     flexDirection: 'row',
+     justifyContent: 'space-between',
    },
 });
 

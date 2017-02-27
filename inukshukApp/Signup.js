144a3a0e2b93df0531ed2d2793b78a3fceac99ae
nanstc <nanstchen@gmail.com>
2017-02-27 09:51:19 -0800

    converted request error handling to ES6 syntax

--- inukshukApp/Signup.js
+++ inukshukApp/Signup.js
@@ -33,51 +33,44 @@ export default class SignUp extends Component {
         };
     }
     execute() {
-
-        this._execute();
-        Alert.alert(
-          'Success!',
-          'Your account has been created!',
-          [
-            {text: 'OK', onPress: () => console.log('OK Pressed')},
-          ],
-          { cancelable: false }
-        )
-    }
-    async _execute() {
-        try {
-            let response = await
-            fetch('http://192.168.1.73:8080/users', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    id: 0,
-                    userName: this.state.userName,
-                    firstName: this.state.fName,
-                    lastName: this.state.lName,
-                    email: this.state.email,
-                    phoneNumber: this.state.phoneNumber,
-                })
-            });
-
-            let responseJson = await response.json();
-            return responseJson.users;
-        }
-        catch(error) {
+        fetch('http://192.168.1.73:8080/users', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                id: 0,
+                userName: this.state.userName,
+                firstName: this.state.fName,
+                lastName: this.state.lName,
+                email: this.state.email,
+                phoneNumber: this.state.phoneNumber,
+            })
+        })
+        .then(handleErrors)
+        .then(function(response) {
             Alert.alert(
-                          'Invalid contact info',
-                          'Please ensure your contact info is correct',
-                          [
-                            {text: 'OK', onPress: () => console.log('OK Pressed')},
-                          ],
-                          { cancelable: false }
-                        )
-        }
+              'Success!',
+              'Your account has been created!',
+              [
+                {text: 'OK', onPress: () => console.log('OK Pressed')},
+              ],
+              { cancelable: false }
+            )
+            return responseJson.users;
 
-    };
+        }).catch(function(error) {
+            Alert.alert(
+              'Invalid Contact Info',
+              error.message,
+              [
+                {text: 'OK', onPress: () => console.log('OK Pressed')},
+              ],
+              { cancelable: false }
+            )
+        })
+    }
     render() {
         return (
         <View style = { {padding: 10}}>
@@ -92,6 +85,17 @@ export default class SignUp extends Component {
         </View>
         );
     }
+}
+
+function handleErrors(response) {
+    if (!response.ok) {
+        if (response.status == 401)
+            throw Error("Please enter valid email address and phone number");
+        }
+        else if (response.status == 422) {
+            throw Error("The email is already registered");
+        }
+    return response;
 }
 
 const styles = StyleSheet.create({

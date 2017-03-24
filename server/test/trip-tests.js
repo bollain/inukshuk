5a9e67828943cb9f915e03b878820fbbaed18bd9
bollain <bollain@gmail.com>
2017-03-23 18:06:44 -0700

    Starting trip tests

---
+++ server/test/trip-tests.js
@@ -0,0 +1,7 @@
+/* eslint-env mocha */
+/* eslint-disable handle-callback-err: "error" */
+// var chai = require('chai')
+// var chaiHttp = require('chai-http')
+// var index = require('../index.js')
+// var should = chai.should() // eslint-disable-line
+// var Trip = require('../models/Trip')

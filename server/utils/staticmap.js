63b82dd390949213af0b91e5af164acda4771d6e
bollain <bollain@gmail.com>
2017-04-04 22:45:46 -0700

    Sample up to 50 coordinates ONLY

--- server/utils/staticmap.js
+++ server/utils/staticmap.js
@@ -6,21 +6,22 @@ var base = 'http://maps.google.com/maps/api/staticmap?size=512x512&maptype=hybri
 module.exports.generateStaticMapURL = function (trip) {
   // The first one is always lat, the second lon
   var result = base
-  var numberOfCrumbs = trip.breadCrumbs.length - 1 // So it is 0 indexed
-  var crumbsLimit = 50
-  while (crumbsLimit >= 0 && numberOfCrumbs >= 0) {
-    var coordinates = trip.breadCrumbs[numberOfCrumbs].coordinates
-    var lat = coordinates[0]
-    var lon = coordinates[1]
+  var coordinates
+  var lat
+  var lon
+  // Only sample up to 50 points
+  var interval = Math.max(Math.ceil(trip.breadCrumbs.length / 50), 1)
+  for (var i = 0; i < trip.breadCrumbs.length; i += interval) {
+    coordinates = trip.breadCrumbs[i].coordinates
+    lat = coordinates[0]
+    lon = coordinates[1]
     result = result + '|' + lat + ',' + lon
-    crumbsLimit--
-    numberOfCrumbs--
   }
   // Now add thhe markers
   var startingCoordinates = trip.startingLocation.coordinates
   var endingCoordinates = trip.endingLocation.coordinates
-  var startMarker = '&markers=color:blue|label:S|' + startingCoordinates[0] + ',' + startingCoordinates[1]
-  var finishMarker = '&markers=color:blue|label:F|' + endingCoordinates[0] + ',' + endingCoordinates[1]
+  var startMarker = '&markers=color:green|label:S|' + startingCoordinates[0] + ',' + startingCoordinates[1]
+  var finishMarker = '&markers=color:red|label:F|' + endingCoordinates[0] + ',' + endingCoordinates[1]
 
   result = result + '&' + startMarker + '&' + finishMarker + '&' + config.GOOGLE_API_TOKEN
   return result
